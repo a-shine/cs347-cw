25124d04d69941336ac808d6bd73cdfda56a6483
eryljoy <eryl.joy@warwick.ac.uk>
2022-03-17 13:24:22 +0000

    for alex

--- persistence_test.go
+++ persistence_test.go
@@ -151,7 +151,7 @@ func checkPersistence(overlayInterface node.Overlay) {
 	fmt.Println("Retreiver address: ", peer.Node().SocketAddr())
 
 	for {
-		if len(peer.Node().KnownHosts()) > 1 { // wait for the requester to connect to a node in the netwrork
+		if len(peer.Node().KnownHosts()) > 0 { // wait for the requester to connect to a node in the netwrork
 			fmt.Println(peer.Node().KnownHosts())
 			fmt.Println("Connecting to Network...")
 			break
