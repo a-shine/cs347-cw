4d9badc53c98ff48a8eded1a067739f0de59bfd1
Zachary Pepin <zachary.e.pepin@gmail.com>
2022-03-23 09:32:23 +0000

    Commented and cleaned overlay.go

--- pcg/overlay.go
+++ pcg/overlay.go
@@ -9,21 +9,18 @@ import (
 	"github.com/a-shine/butter/node"
 )
 
-// Peer implements an overlay node which is described in the Butter node overlay interface
+// Peer implements an overlay node, as described in the Butter node overlay interface.
 type Peer struct {
 	node           *node.Node
-	maxStorage     uint64 //TODO will be in node
-	currentStorage uint64 //TODO will be in node
+	maxStorage     uint64 
+	currentStorage uint64
 	storage        map[[32]byte]*Group
 }
 
-// --- Constructor ---
-
-// NewPCG overlay node
+// NewPCG constructs a Peer.
 func NewPCG(node *node.Node, maxMemoryMb uint64) Peer {
 	maxMemory := MbToBytes(maxMemoryMb)
 	maxStorage := MaxStorage(maxMemory)
-	//fmt.Println("Max storage:", maxStorage)
 	return Peer{
 		node:       node,
 		maxStorage: maxStorage,
@@ -31,17 +28,20 @@ func NewPCG(node *node.Node, maxMemoryMb uint64) Peer {
 	}
 }
 
-// --- Getters ---
-
-func (p *Peer) Node() *node.Node { // Required to implement the Butter overlay interface
+// Node gets the underyling Node in the Peer object.
+// Required to implement the Butter overlay interface.
+func (p *Peer) Node() *node.Node { 
 	return p.node
 }
 
-func (p *Peer) AvailableStorage() uint64 { // Required to implement the Butter overlay interface
+// AvailableStorage gets the remaining storage available to a Peer.
+// Required to implement the Butter overlay interface.
+func (p *Peer) AvailableStorage() uint64 { 
 	return p.maxStorage - p.currentStorage
 }
 
-// Group from node's storage by its UUID. If the node is not part of that group, returns nil with an error.
+// Group gets a Peer's Group by its UUID. 
+// If the Peer is not a participant of the passed UUID's Group, returns nil with an error.
 func (p *Peer) Group(id string) (*Group, error) {
 	var hash [32]byte
 	data, _ := hex.DecodeString(id)
@@ -52,28 +52,26 @@ func (p *Peer) Group(id string) (*Group, error) {
 	return nil, errors.New("block not found")
 }
 
-// Groups from node's storage by their UUIDs
+// Groups gets a Peer's Groups' UUIDs.
 func (p *Peer) Groups() map[[32]byte]*Group {
 	return p.storage
 }
 
-// --- Node Group creation and joining ---
 
-// CreateGroup with node. A UUID is generated for every bit of information added to the network (no update
-// functionality yet!). Returns the UUID of the new group as a string.
+// CreateGroup creates a Group storing the passed data, and assigns the Peer as its first participant.
+// The Group UUID is generated from the stored information.
+// Returns the UUID of the new group as a string.
 func (p *Peer) CreateGroup(data string) string {
-	// TODO: check if node memory (allocated by the user on initialization) is full - node API
 	var formattedData [4096]byte
 	copy(formattedData[:], data)
 	hsha2 := sha256.Sum256(formattedData[:])
 	p.storage[hsha2] = NewGroup(formattedData, p.node.SocketAddr())
-	p.currentStorage += 4096 //TODO
+	p.currentStorage += 4096
 	return fmt.Sprintf("%x", hsha2)
 }
 
-// JoinGroup from a node
+// JoinGroup assigns the passed Group to a Peer's Group.
 func (p *Peer) JoinGroup(g Group) {
-	//TODO: UPDATE TO GROUP DIGEST WHEN GROUP MODIFIED
 	hsha2 := sha256.Sum256(g.Data[:])
 	err := g.AddParticipant(p.node.SocketAddr())
 	if err != nil {
@@ -82,9 +80,8 @@ func (p *Peer) JoinGroup(g Group) {
 	p.storage[hsha2] = &g
 }
 
-// --- Encoders ---
 
-// String of node's groups
+// String returns a string representation of a Peer.
 func (p *Peer) String() string {
 	str := ""
 	for _, g := range p.Groups() {
